fa20e242c4ece1be31ff2a9ab3151ba3267ca421
Keshar <paudelkeshar2055@gmail.com>
2019-04-04 17:34:16 +0545

    Real time data saveed in real time database in firebase

--- app/src/main/java/com/example/keshar/sendrealtimedatainfirebasedatabase/MainActivity.java
+++ app/src/main/java/com/example/keshar/sendrealtimedatainfirebasedatabase/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.keshar.sendrealtimedatainfirebasedatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etName,etValue;
+    private Button btnSend;
+    private Firebase firebaseUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        etName=findViewById(R.id.et_name);
+        etValue=findViewById(R.id.name);
+        btnSend=findViewById(R.id.btn_send);
+        firebaseUrl=new Firebase("https://applicationdemo-eff34.firebaseio.com/");
+
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value=etValue.getText().toString().trim();
+                String name=etName.getText().toString().trim();
+                Firebase firebase=firebaseUrl.child("Name").child(name);
+                firebase.setValue(value);
+
+            }
+        });
+
     }
 }
